873e1597ed42af84d65b56d5b782de76f891d5b4
Joyce Zhang <jzzhang.dev@gmail.com>
2023-04-18 20:25:38 -0400

    Change the current traffic light

--- 8kyu/thinkful-logic-drills.js
+++ 8kyu/thinkful-logic-drills.js
@@ -1,3 +1,5 @@
+//Thinkful logic Drills
+
 // You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
 
 // Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
@@ -5,10 +7,9 @@
 // For example, when the input is green, output should be yellow.
 
 function updateLight(current) {
-if(current ==='green'){
-  return 'yellow'
-  }else if(current ==='yellow'){
-  return 'red'
-  }else
-  return 'green'
+  if (current === "green") {
+    return "yellow";
+  } else if (current === "yellow") {
+    return "red";
+  } else return "green";
 }
