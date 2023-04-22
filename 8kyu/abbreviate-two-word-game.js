d5933701db097ba9a495672b745dfb1caa538e55
Joyce Zhang <jzzhang.dev@gmail.com>
2023-04-21 20:03:56 -0400

    Take a string of a name and abbreviate them into initials

---
+++ 8kyu/abbreviate-two-word-game.js
@@ -0,0 +1,16 @@
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// patrick feeney => P.F
+
+function abbrevName(name){
+    let newArr = name.split(" ")
+    return `${newArr[0].charAt(0).toUpperCase()}.${newArr[1].charAt(0).toUpperCase()}`
+    
+    }
+    
