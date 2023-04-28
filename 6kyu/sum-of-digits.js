585f2cd6431698e65f27db409209d81b62f3e670
Joyce Zhang <jzzhang.dev@gmail.com>
2023-04-27 23:09:57 -0400

    Find the digital root of n until it's a single digit

---
+++ 6kyu/sum-of-digits.js
@@ -0,0 +1,15 @@
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digital_root(n) {
+    return (n - 1) % 9 + 1;
+  }
+
+  //I had to get help for this one. There's a formula in which to calculate the digital squareroot of n is: "the digital root of a positive integer n is equal to n mod 9 if n mod 9 is not zero. If n mod 9 is zero, then the digital root is 9."
