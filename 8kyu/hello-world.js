e770920b41970d75b5648bb185832d22f8a09a28
Joyce Zhang <jzzhang.dev@gmail.com>
2023-07-01 22:02:16 -0400

    Very simple. Just create a function to return the sentence 'Hello World'

--- 8kyu/hello-world.js
+++ 8kyu/hello-world.js
@@ -4,6 +4,7 @@
 // Sure, this is about as easy as it gets. But how clever can you be to create the most creative "hello world" you can think of? What is a "hello world" solution you would want to show your friends?
 
 // Write a function "greet" that returns "hello world!"
+
 function greet(){
     let takeThis = "hello world!";
     return takeThis;
