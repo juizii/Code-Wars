3d060a8c65968839262bb6ed0e921303d161e790
Joyce Zhang <jzzhang.dev@gmail.com>
2023-07-16 19:12:20 -0400

    Convert a given string into an array

--- 8kyu/convert-string-to-array.js
+++ 8kyu/convert-string-to-array.js
@@ -5,6 +5,7 @@
 
 // "I love arrays they are my favorite" ==> ["I", "love", "arrays", "they", "are", "my", "favorite"]
 
+
 function stringToArray(string){
     return string.trim().split(' ');
    
