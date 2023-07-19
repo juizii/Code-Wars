7b87f41ff736f7d5e8d69faf8eee5d4a89904dc9
Joyce Zhang <jzzhang.dev@gmail.com>
2023-07-18 22:49:24 -0400

    Return true if your test avg did better than the entire class avg

--- 8kyu/how-good-are-you-really.js
+++ 8kyu/how-good-are-you-really.js
@@ -9,6 +9,7 @@
 // Note:
 // Your points are not included in the array of your class's points. For calculating the average point you may add your point to the given array!
 
+
 function betterThanAverage(classPoints, yourPoints) {
     let avg = 0;
     for(let i=0; i < classPoints.length; i++){
