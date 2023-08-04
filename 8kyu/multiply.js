6cbce28b47747e5979c5b66e80bc9a399396d035
Joyce Zhang <jzzhang.dev@gmail.com>
2023-08-04 13:51:01 -0400

    Create a function that multiplies two given integers

--- 8kyu/multiply.js
+++ 8kyu/multiply.js
@@ -1,3 +1,5 @@
+//create a function that will multiply two given integers and return the product. Remember to call the function.
+
 function multiply(a, b){
     let total= a * b;
     return total;
